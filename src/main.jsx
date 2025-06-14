fab1d128b21fcd6e3bdc81a081f58a83528b4dc1
Gonzalo Martinez <150065620+GonzaloMartinezz@users.noreply.github.com>
2025-06-14 13:32:08 -0300

    estoy trantando de ver sobre nosotros ,tengo que ver como vincularlo , no encontre todavia la solucion

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
+import NavBar from './components/NavBar.jsx';
+import SobreNosotros from './components/SobreNosotros.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
