d13a0ea8f0d6883b2c04d6873e345e8cc64ed114
LautaroLeall <lautaroleal4@gmail.com>
2025-07-02 14:10:07 -0300

    Deploy Imagenes

--- src/components/SobreNosotros.jsx
+++ src/components/SobreNosotros.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/SobreNosotros.css';
-import sobreNosotros from '../../public/sobre-nosotros.jpg';
+import sobreNosotros from '/public/sobre-nosotros.jpg';
 
 const SobreNosotros = () => {
     return (
