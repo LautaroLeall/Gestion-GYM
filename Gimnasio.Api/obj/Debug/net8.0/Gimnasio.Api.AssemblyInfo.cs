4d3793dea76c69ee5df77dd40a2f2a322f0b28fd
LautaroLeall <lautaroleal4@gmail.com>
2025-11-04 01:21:27 -0300

    info .cs (back)

--- Gimnasio.Api/obj/Debug/net8.0/Gimnasio.Api.AssemblyInfo.cs
+++ Gimnasio.Api/obj/Debug/net8.0/Gimnasio.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gimnasio.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b8c694e350b095158c34bc83e53110b0c3e9dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b1ceff885de20aa1753133e680d9fc77d27ffd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gimnasio.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gimnasio.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
