12e71c75aec0d562cdbac57e7e0809b0ad96b615
LautaroLeall <lautaroleal4@gmail.com>
2025-12-08 12:07:30 -0300

    vite config (front)

--- ClientApp/vite.config.js
+++ ClientApp/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Configuración de Vite para un proyecto React. Esta configuración
+// Configuración de Vite
 // incluye el plugin de React y un proxy para redirigir las peticiones
-// de API en desarrollo al servidor ASP.NET Core (puerto 5000 por defecto).
+// de API en desarrollo al servidor ASP.NET Core (puerto 5000).
 
 export default defineConfig({
   plugins: [react()],
